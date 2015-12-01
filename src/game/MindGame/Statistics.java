6395c5b80fa832004355154a8d2cd362fa57aa41
YuraNedelko <nedelkoyura@gmail.com>
2015-12-01 19:45:06 +0200

    Update Statistics.java

--- src/game/MindGame/Statistics.java
+++ src/game/MindGame/Statistics.java
@@ -1,7 +1,8 @@
 package game.MindGame;
 
 import android.content.Context;
-
+import android.widget.TextView;
+import android.app.Activity;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -13,7 +14,6 @@ import java.util.TreeMap;
 
 
 public class Statistics {
-
     private static String fileName = "File.txt";
     Context c;
     private Map<Integer, String> users = new TreeMap<Integer, String>();
@@ -22,9 +22,9 @@ public class Statistics {
     {
         this.c = context;
         addUser("Paul", 213);
-        addUser("Dean", 11);
+        addUser("Anna", 11);
         addUser("Sam", 413);
-        addUser("Jane", 113);
+        addUser("Pete", 113);
         addUser("Bill", 813);
         addUser("Carl", 5);
     }
@@ -67,5 +67,53 @@ public class Statistics {
             e.printStackTrace();
         }
     }
+
+    public void readStatistic(Activity activity) {
+        final int N = 10;
+
+        TextView t  = (TextView) activity.findViewById(R.id.TextView0);
+        TextView t1 = (TextView) activity.findViewById(R.id.TextView1);
+        TextView t2 = (TextView) activity.findViewById(R.id.TextView2);
+        TextView t3 = (TextView) activity.findViewById(R.id.TextView3);
+        TextView t4 = (TextView) activity.findViewById(R.id.TextView4);
+        TextView t5 = (TextView) activity.findViewById(R.id.TextView5);
+        TextView t6 = (TextView) activity.findViewById(R.id.TextView6);
+        TextView t7 = (TextView) activity.findViewById(R.id.TextView7);
+        TextView t8 = (TextView) activity.findViewById(R.id.TextView8);
+        TextView t9 = (TextView) activity.findViewById(R.id.TextView9);
+
+        Object[] arr = new Object[N];
+        arr[0] = t;        arr[1] = t1;        arr[2] = t2;        arr[3] = t3;        arr[4] = t4;
+        arr[5] = t5;       arr[6] = t6;        arr[7] = t7;        arr[8] = t8;        arr[9] = t9;
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(c.openFileInput(fileName)));
+            String str, str1;
+            for (int i = 0; i <= N - 1; i += 2) {
+                if ((str = br.readLine()) != null) {
+                    for (int k = 5 - str.length() - 1; k >= 0; k--)
+                        str = "0" + str;
+                    TextView text = (TextView) arr[i + 1];
+                    text.setText(str);
+                } else {
+                    TextView text = (TextView) arr[i + 1];
+                    text.setText("");
+                }
+                if ((str1 = br.readLine()) != null) {
+
+                    TextView text1 = (TextView) arr[i];
+                    text1.setText(str1);
+                } else {
+                    TextView text1 = (TextView) arr[i];
+                    text1.setText("");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 
