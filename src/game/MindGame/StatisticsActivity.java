f6306e900e334dd9df4a6050d5bf1ba31ec0bf48
YuraNedelko <nedelkoyura@gmail.com>
2015-12-01 21:24:56 +0200

    Update StatisticsActivity.java

--- src/game/MindGame/StatisticsActivity.java
+++ src/game/MindGame/StatisticsActivity.java
@@ -2,35 +2,28 @@ package game.MindGame;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
 
 
 public class StatisticsActivity extends Activity {
 
     private static String fileName = "File.txt";
+    Statistics statistics = new Statistics(this);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics);
-        Statistics statistics = new Statistics(this);
-        statistics.readStatistic(this);
+        statistics.Test();
+        statistics.setActivity(this);
+        statistics.readStatistic();
         getActionBar().hide();
     }
 
-
     public void deleteStatistic(View view) {
-        final Activity c = this;
-        final Statistics statistics = new Statistics(this);
         LayoutInflater li = LayoutInflater.from(this);
         View view1 = li.inflate(R.layout.dialoguewindow1, null);
 
@@ -40,15 +33,7 @@ public class StatisticsActivity extends Activity {
 
         alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                try {
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE)));
-                    bw.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                statistics.readStatistic(c);
+               statistics.deleteStatistics();
             }
         })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -61,5 +46,3 @@ public class StatisticsActivity extends Activity {
         alertDialog.show();
     }
 }
-
-
